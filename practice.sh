7fcf6b753dab79f40117a04c97467273324b8b72
sreedhar <sreedhar70755@gmail.com>
2023-11-09 19:21:21 +0530

    tf

---
+++ practice.sh
@@ -0,0 +1,21 @@
+comp=1
+if [ $comp == 1 ]; then
+        echo ifel
+fi
+
+
+if [ $comp == 2 ]; then
+        echo executing if
+else
+        echo execution else
+fi
+
+
+if [ $comp == 3 ]; then
+  echo 1stif executing
+  elif [ $comp == 1]; then
+    echo elifis executing
+  else
+    echo last one
+    fi
+
