66d0ad9780e148f1f3cdfe407f3daea1ed0bf88e
sreedhar <sreedhar70755@gmail.com>
2023-11-10 16:16:53 +0530

    tf

--- practice.sh
+++ practice.sh
@@ -1,26 +1,26 @@
-#comp=1
-#if [ $comp == 1 ]; then
-#        echo ifel
-#fi
-#
-#
-#if [ $comp -gt 2 ]; then
-#        echo executing if
-#else
-#        echo execution else
-#fi
-#
-#
-#if [ $comp -gt 3 ]; then
-#  echo 1stif executing
-#  elif [ $comp == 2 ]; then
-#    echo elifis executing
-#  else
-#    echo last one
-#    fi
+comp=1
+if [ $comp == 1 ]; then
+        echo ifel
+fi
 
 
-#echo service file
+if [ $comp -gt 2 ]; then
+        echo executing if
+else
+        echo execution else
+fi
+
+
+if [ $comp -gt 3 ]; then
+  echo 1stif executing
+  elif [ $comp == 2 ]; then
+    echo elifis executing
+  else
+    echo last one
+    fi
+
+
+echo service file
 
 a=10
 
